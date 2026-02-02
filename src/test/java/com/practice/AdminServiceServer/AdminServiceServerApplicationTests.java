b6280b7105373ac13c809b8d396fcd97920e78e7
kishorekasim <kishorehgsk9@gamil.com>
2026-02-02 11:44:16 +0530

    Initial Commit

---
+++ src/test/java/com/practice/AdminServiceServer/AdminServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.AdminServiceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
